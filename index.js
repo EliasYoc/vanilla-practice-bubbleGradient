d16659a3f3d3c4f74fa482d99f758f4492fbdfa1
EliasYoc <eliyoc_97@hotmail.com>
2022-06-09 13:19:37 -0500

    feat function to rotate background

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const d = document;
 const $range = d.getElementById("range"),
   $span = d.getElementById("value");
-// to avoid to style each bubble I used styleSheets
+// to avoid to style each bubble, I used styleSheets
 const cssRules = d.styleSheets[0].cssRules;
 let myRule = null;
 for (const rule of cssRules) {
@@ -11,5 +11,16 @@ $range.addEventListener("input", (e) => {
   $span.textContent = $range.value;
   myRule.style.borderRadius = `${$range.value}px`;
 });
-
 $span.textContent = $range.value;
+
+// ROTATE
+const $btnRotate = d.getElementById("rotate");
+const $parentGradient = d.querySelector(".parent");
+let deg = 90;
+const rotateGradientBackground = () => {
+  if (deg > 360) deg = 90;
+  $parentGradient.style.setProperty("--a", `${deg}deg`);
+  console.log(deg);
+  deg += 90;
+};
+$btnRotate.addEventListener("click", () => rotateGradientBackground());
